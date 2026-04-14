7a1041943208e33d09bb8a10e6368f3d4939fc28
lbeltran <leticia.beltran94@gmail.com>
2026-04-13 20:29:29 -0500

    fix layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ const Layout = () => {
   return (
     <div className="bg-white dark:bg-gray-900 min-h-dvh flex flex-col">
       <Navbar />
-      <div className="mx-4 lg:mx-10 xl:mx-16 2xl:mx-60 my-4">
+      <div className="mx-4 lg:mx-12 xl:mx-22 2xl:mx-64 my-4">
         <Outlet />
       </div>
     </div>
